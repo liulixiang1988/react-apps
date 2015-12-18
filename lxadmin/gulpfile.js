943cf999684b1de5ffc24b2759c40b80892dc54f
liulixiang1988 <liulixiang1988@gmail.com>
2015-12-18 14:44:46 +0800

    添加bundle

--- lxadmin/gulpfile.js
+++ lxadmin/gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require("gulp");
 var connect = require("gulp-connect"); //Runs a local dev server
 var open = require("gulp-open"); //Open a URL in a web browser
+var browserify = require('browserify'); //Bundle JS
+var reactify = require('reactify'); //transforms React JSX to JS
+var source = require('vinyl-source-stream'); //Use conventional text streams with Gulp
 
 var config = {
     port: 9000,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 };
 
@@ -34,8 +39,19 @@ gulp.task('html', function () {
         .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
-    gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function(){
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console)) //出错机制
+        .pipe(source('bundle.js')) //browserify的输出名字
+        .pipe(gulp.dest(config.paths.dist+'/scripts')) //gulp拷贝目录地址
+        .pipe(connect.reload()); //重载服务器
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('watch', function(){
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
---
+++ lxadmin/src/main.js
@@ -0,0 +1,3 @@
+var App = console.log("Hello world from browserify");
+
+module.exports = App;
