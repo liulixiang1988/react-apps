2b3acb175234f8cba7e6678ae48a66dbae8717b0
liulixiang1988 <liulixiang1988@gmail.com>
2015-12-21 19:38:14 +0800

    add bootstrap library

--- lxadmin/gulpfile.js
+++ lxadmin/gulpfile.js
@@ -6,6 +6,7 @@ var open = require("gulp-open"); //Open a URL in a web browser
 var browserify = require('browserify'); //Bundle JS
 var reactify = require('reactify'); //transforms React JSX to JS
 var source = require('vinyl-source-stream'); //Use conventional text streams with Gulp
+var concat = require('gulp-concat'); //Concatenates files
 
 var config = {
     port: 9000,
@@ -13,6 +14,10 @@ var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
         dist: './dist',
         mainJs: './src/main.js'
     }
@@ -28,17 +33,20 @@ gulp.task("connect", function(){
     });
 });
 
+//Open url in web browser
 gulp.task('open', ['connect'], function () {
     gulp.src('dist/index.html')
         .pipe(open('', { url: config.devBaseUrl+':'+config.port+'/'}));
 });
 
+//Copy html to dist folder
 gulp.task('html', function () {
     gulp.src(config.paths.html)
         .pipe(gulp.dest(config.paths.dist))
         .pipe(connect.reload());
 });
 
+//Compile js and copy js files to dist folder
 gulp.task('js', function(){
     browserify(config.paths.mainJs)
         .transform(reactify)
@@ -49,9 +57,20 @@ gulp.task('js', function(){
         .pipe(connect.reload()); //重载服务器
 });
 
+//Concat css files to dist folder
+gulp.task('css', function () {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist+'/css'))
+        .pipe(connect.reload());
+});
+
+//Watch file changes
 gulp.task('watch', function(){
     gulp.watch(config.paths.html, ['html']);
     gulp.watch(config.paths.js, ['js']);
+    gulp.watch(config.paths.css, ['css']);
 });
 
-gulp.task('default', ['html', 'js', 'open', 'watch']);
+//Default css
+gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
--- lxadmin/src/main.js
+++ lxadmin/src/main.js
@@ -1,3 +1,5 @@
+$ = jQuery = require('jquery'); //全局变量
+
 var App = console.log("Hello world from browserify");
 
 module.exports = App;
