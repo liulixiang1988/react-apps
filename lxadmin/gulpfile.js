1f68247a3aa30668f8af91b9851be6a90c16052b
liulixiang1988 <liulixiang1988@gmail.com>
2015-12-22 19:59:52 +0800

    update open
    open file in dev server

--- lxadmin/gulpfile.js
+++ lxadmin/gulpfile.js
@@ -37,7 +37,7 @@ gulp.task("connect", function(){
 //Open url in web browser
 gulp.task('open', ['connect'], function () {
     gulp.src('dist/index.html')
-        .pipe(open('', { url: config.devBaseUrl+':'+config.port+'/'}));
+        .pipe(open({ uri: config.devBaseUrl+':'+config.port+'/', app: 'Google Chrome'}));
 });
 
 //Copy html to dist folder
