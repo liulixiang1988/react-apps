f60ad4e9f74179b42f13494093c6cfc68d003049
liulixiang1988 <liulixiang1988@gmail.com>
2016-03-04 11:46:47 +0800

    weather project

---
+++ book_demos/WeatherProject/Forecast.js
@@ -0,0 +1,47 @@
+'user strict';
+
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+const styles = StyleSheet.create({
+  flex: {
+    alignItems: 'center',
+  },
+  bigText: {
+    flex: 2,
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF',
+  },
+  mainText: {
+    flex: 1,
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#FFFFFF',
+  }
+});
+
+
+export default class Forecast extends Component {
+  render() {
+    return (
+      <View style={styles.flex}>
+        <Text style={styles.bigText}>
+          {this.props.main}
+        </Text>
+        <Text style={styles.mainText}>
+          当前天气：{this.props.description}
+        </Text>
+        <Text style={styles.bigText}>
+          {this.props.temp}°C
+        </Text>
+      </View>
+    );
+  }
+}
--- book_demos/WeatherProject/WeatherProject.js
+++ book_demos/WeatherProject/WeatherProject.js
@@ -2,6 +2,7 @@
 
 import React, {
   Component,
+  Image,
   StyleSheet,
   Text,
   TextInput,
@@ -9,23 +10,51 @@ import React, {
 } from 'react-native';
 
 
+import Forecast from './Forecast';
+
+const baseFontSize = 16;
+
 const styles = StyleSheet.create({
+  backdrop: {
+    flex: 1,
+    flexDirection: 'column',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'flex-start',
+    padding: 30,
   },
-  input: {
-    fontSize: 20,
-    borderWidth: 2,
-    height: 40,
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: '#000000',
+    opacity: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center',
   },
+  cityContainer: {
+    flex: 1,
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3,
+  },
+  cityName: {
+    width: 50,
+    height: baseFontSize,
+  },
+  mainText: {
+    flex: 1,
+    fontSize: baseFontSize,
+    color: '#FFFFFF',
+  }
+
 });
 
 
@@ -34,24 +63,80 @@ export default class WeatherProject extends Component {
   constructor(props){
     super(props);
     this.state = {
-      city: ''
+      city: '',
+      forecast: null
     }
   }
 
   _handleTextChange(event){
-    console.log(event.nativeEvent.text);
+    let city = event.nativeEvent.text;
+    var city_code = '';
+    console.log(city);
     this.setState({
-      city: event.nativeEvent.text
+      city: city
     });
+    fetch('https://api.heweather.com/x3/citylist?search=allchina&key=ef29baab02f049adaa4ce2afba6f29b9')
+      .then((response)=>response.json())
+      .then((responseJson)=>{
+        console.log(responseJson);
+        for(var item of responseJson.city_info){
+          if (item.city == city){
+            console.log("city");
+            this._getWeatherInfo(item.id);
+          }
+        }
+      })
+      .catch((error)=>{
+        console.warn(error);
+      });
+  }
+
+  _getWeatherInfo(city_code){
+    let url = 'https://api.heweather.com/x3/weather?cityid='+city_code+'&key=ef29baab02f049adaa4ce2afba6f29b9';
+    fetch(url)
+      .then((response)=>response.json())
+      .then((responseJson)=>{
+        let weatherInfo = responseJson["HeWeather data service 3.0"][0];
+        console.log(weatherInfo);
+        this.setState({
+          forecast:{
+            main: weatherInfo.daily_forecast[0].cond.txt_n,
+            description: weatherInfo.aqi.city.qlty,
+            temp: weatherInfo.daily_forecast[0].tmp.min+'~'+weatherInfo.daily_forecast[0].tmp.max
+          }
+        });
+      })
+      .catch((error)=>{console.warn(error);});
   }
 
   render() {
+    var content = null;
+    if (this.state.forecast !== null){
+      content = <Forecast 
+              main={this.state.forecast.main}
+              description={this.state.forecast.description}
+              temp={this.state.forecast.temp}/>;
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          城市：{this.state.city}
-        </Text>
-        <TextInput style={styles.input} onSubmitEditing={this._handleTextChange.bind(this)}/>
+        <Image source={require('image!flowers')}
+               resizeMode='cover'
+               style={styles.backdrop}>
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <View style={styles.cityContainer}>
+                <TextInput
+                  style={[styles.cityName, styles.mainText]}
+                  returnKeyType='go'
+                  onSubmitEditing={this._handleTextChange.bind(this)}/>
+              </View>
+              <Text style={styles.mainText}>
+                天气
+              </Text>
+            </View>
+            {content}
+          </View>
+        </Image>
       </View>
     );
   }
